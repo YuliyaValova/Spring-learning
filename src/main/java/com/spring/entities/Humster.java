ef6d904e57d68252e050be729736f5683ee5aebd
YuliyaValova <Lulkaval@gmail.com>
2022-01-11 19:21:48 +0300

    Beans and context

---
+++ src/main/java/com/spring/entities/Humster.java
@@ -0,0 +1,16 @@
+package com.spring.entities;
+
+import org.springframework.stereotype.Component;
+
+
+public class Humster {
+    private String name = "Hamyak";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
